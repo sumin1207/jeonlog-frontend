a69c8f0c05759da39a98ced244d379cae710a9ab
chin00011122 <chin0928@naver.com>
2025-07-25 17:17:42 +0900

    슬라이딩, 하단바 틀 생성

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,25 @@
-import { useRouter } from "expo-router";
-import { Button } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import HorizontalSliding from "@/components/HorizontalSliding";
 
-export default function IndexPage() {}
+export default function IndexHome() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>요즘 뜨고 있는 전시</Text>
+      <HorizontalSliding />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "condensed",
+    marginBottom: 10,
+  },
+});
